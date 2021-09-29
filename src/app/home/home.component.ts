f7647ebebb2a18412e968e5134b81ba5fe36a317
Guilherme Carvalho Vieira Santos <guilherme.santos536@etec.sp.gov.br>
2021-09-29 19:25:30 -0300

    [Angular] Exercício 4 - Logica

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+nomeProduto: string = 'Curso de Angular';
+idProduto: number = 123;
+precoProduto: number = 2.59;
+promocao: boolean = true;
+anuncio: string = `O ${this.nomeProduto} está em promoção!`;
+
+
+  constructor() {
+    // variáveis de string com concatenação
+    //this.anuncio = 'O ' + this.nomeProduto + 'está em promoção!';
+
+    console.log('Nome do Produto', this.nomeProduto)
+    console.log('Anuncio: ', this.anuncio);
+    console.log('ID: ', this.idProduto);
+    console.log('Preço: ', this.precoProduto);
+    console.log('Promoção: ', this.promocao);
+
+
+    let idade = 10;
+    function imprimeIdade(){
+      for (let idade = 30; idade <=40; idade++){
+      console.log('Idade dentro do For é: ', idade);
+      }
+      console.log('Idade fora do for: ', idade);
+    }
+
+    imprimeIdade();
+
+   }
 
   ngOnInit(): void {
   }
